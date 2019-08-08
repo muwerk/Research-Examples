dd000e7bca38c1443b61bbaa3295846f9cb6a049
Dominik Schlösser <dsc@dosc.net>
2019-08-08 17:37:27 +0200

    DHTxx example

---
+++ dht/README.md
@@ -0,0 +1,12 @@
+## Temperature and humidity with DHT11, DHT12, or DHT22 example
+
+A simple example demonstrating muwerk's scheduler, munet interface to WLAN, NTP, OTA and MQTT, and simple hardware (switch and led).
+
+<img src="https://github.com/muwerk/mupplets/blob/master/Resources/dht.png" width="30%" height="30%">
+Hardware: 10kΩ, DHT22 sensor.
+
+## Documentation
+
+* [muwerk scheduler and messaging](https://github.com/muwerk/muwerk)
+* [munet networking and mqtt](https://github.com/muwerk/munet)
+* [documentation of hardware mupplets and supported MQTT messages](https://github.com/muwerk/mupplets)
---
+++ dht/src/dht.cpp
@@ -0,0 +1,56 @@
+#define USE_SERIAL_DBG 1
+
+#include "platform.h"
+#include "scheduler.h"
+#include "net.h"
+#include "mqtt.h"
+#include "ota.h"
+
+#include "dht.h"
+
+void appLoop();
+
+ustd::Scheduler sched(10,16,32);
+ustd::Net net(LED_BUILTIN);
+ustd::Mqtt mqtt;
+ustd::Ota ota;
+ustd::Dht dht("myDht",D4);
+
+void sensor_messages(String topic, String msg, String originator) {
+#ifdef USE_SERIAL_DBG
+    Serial.println("Switch received: "+topic+"|"+msg);
+#endif
+    if (topic == "myDht/temperature") {
+#ifdef USE_SERIAL_DBG
+        Serial.println("Temperature: "+msg);
+#endif
+    }
+    if (topic == "myDht/humidity") {
+#ifdef USE_SERIAL_DBG
+        Serial.println("Humidity: "+msg);
+#endif
+    }
+}
+
+
+void setup() {
+#ifdef USE_SERIAL_DBG
+    Serial.begin(115200);
+    Serial.println("Startup");
+#endif  // USE_SERIAL_DBG
+    net.begin(&sched);
+    mqtt.begin(&sched);
+    ota.begin(&sched);
+    int tID = sched.add(appLoop, "main", 1000000);
+    dht.begin(&sched);
+
+    sched.subscribe(tID, "myDht/#", sensor_messages);
+}
+
+void appLoop() {
+}
+
+// Never add code to this loop, use appLoop() instead.
+void loop() {
+    sched.loop();
+}
