18ca2cc598a5fe805a844899af2c8f6b6a634726
Dominik Schlösser <dsc@dosc.net>
2019-11-25 13:30:18 +0100

    Working version with OTA

--- gosund-sp1/src/gosund-sp1.cpp
+++ gosund-sp1/src/gosund-sp1.cpp
@@ -1,5 +1,3 @@
-//#define USE_SERIAL_DBG 1
-
 #include "platform.h"
 #include "scheduler.h"
 
@@ -14,56 +12,43 @@
 void appLoop();
 
 ustd::Scheduler sched(10,16,32);
-ustd::Net net(1);
+ustd::Net net(13); // use red led for network connection phase
 ustd::Mqtt mqtt;
 ustd::Ota ota;
 
-ustd::Led led1("Led1",1,false);
-ustd::Led led2("Led2",12,false);
+ustd::Led led1("Led1",1,false); // blue
+ustd::Led led2("Led2",13,false); // red
 ustd::Switch toggleswitch("mySwitch",3, ustd::Switch::Mode::Default, false);
-// Optional IRQ support: (each switch needs unique interruptIndex [0..9])
-// ustd::Switch toggleswitch("mySwitch",D6, ustd::Switch::Mode::Flipflop, false, "mySwitch/switch/IRQ/0", 0, 25);
-ustd::DigitalOut relay("Relay",14,false);
+ustd::DigitalOut relay("Relay",14,true);
 
 void switch_messages(String topic, String msg, String originator) {
-#ifdef USE_SERIAL_DBG
-    Serial.println("Switch received: "+topic+"|"+msg);
-#endif
     if (topic == "mySwitch/switch/state") {
         if (msg=="on") {
             led1.set(true);
-            // sched.publish("relay/Led1/light/set","on");
+            relay.set(true);
         } else if (msg=="off") {
             led1.set(false);
-            // sched.publish("relay/Led1/light/set","off");
-        } else if (msg=="trigger") {
-            led1.setMode(ustd::Led::Mode::Pulse, 50);
+            relay.set(false);
         }
     }
 }
 
 void setup() {
-#ifdef USE_SERIAL_DBG
-    Serial.begin(115200);
-    Serial.println("Startup");
-#endif  // USE_SERIAL_DBG
     net.begin(&sched);
     mqtt.begin(&sched);
     ota.begin(&sched);
-    int tID = sched.add(appLoop, "main", 1000000); // every 1000000 micro sec = once a second call appLoop
+    int tID = sched.add(appLoop, "main", 1000000);
     led1.begin(&sched);
     led2.begin(&sched);
     toggleswitch.begin(&sched);
     toggleswitch.setMode(ustd::Switch::Mode::Flipflop);
     relay.begin(&sched);
 
-    // Use Home Assistant's auto-discovery to register switch and led in HA with names DigiTast, Blaue Led.
-    toggleswitch.registerHomeAssistant("Gosund 01 Switch", "Gosund-SP1 01");
-    led1.registerHomeAssistant("Gosund 01 Led1", "Gosund-SP1 01");
-    led2.registerHomeAssistant("Gosund 01 Led2", "Gosund-SP1 01");
-    relay.registerHomeAssistant("Gosund 01 Relay", "Gosund-SP1 01");
+    toggleswitch.registerHomeAssistant("GS Switch", "Gosund-SP1");
+    led1.registerHomeAssistant("GS Led1", "Gosund-SP1");
+    led2.registerHomeAssistant("GS Led2", "Gosund-SP1");
+    relay.registerHomeAssistant("GS Relay", "Gosund-SP1");
 
-    // led.setMode(led.Mode::Blink,1000);
     sched.subscribe(tID, "mySwitch/switch/state", switch_messages);
 }
 
