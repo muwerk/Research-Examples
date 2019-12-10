1ec1854127731852e7e0a471ede5713e0616b220
Dominik Schlösser <dsc@dosc.net>
2019-12-10 14:32:51 +0100

    initial test tool

---
+++ marklin/src/marklin.cpp
@@ -0,0 +1,44 @@
+#define USE_SERIAL_DBG 1
+
+#include "platform.h"
+#include "scheduler.h"
+
+#include "net.h"
+#include "mqtt.h"
+#include "ota.h"
+
+#include "dcc.h"
+
+void appLoop();
+
+ustd::Scheduler sched(10,16,32);
+ustd::Net net(LED_BUILTIN);
+ustd::Mqtt mqtt;
+ustd::Ota ota;
+
+#ifdef __ESP32__
+ustd::Dcc dcc("Märklin",26,25,21);
+#else
+ustd::Dcc dcc("Märklin",D6,D7,D8);
+#endif
+
+void setup() {
+#ifdef USE_SERIAL_DBG
+    Serial.begin(115200);
+    Serial.println("Startup");
+#endif  // USE_SERIAL_DBG
+    net.begin(&sched);
+    mqtt.begin(&sched);
+    ota.begin(&sched);
+    dcc.begin(&sched);
+
+    /* int tID = */sched.add(appLoop, "main", 1000000); // every 1000000 micro sec = once a second call appLoop
+}
+
+void appLoop() {
+}
+
+// Never add code to this loop, use appLoop() instead.
+void loop() {
+    sched.loop();
+}
