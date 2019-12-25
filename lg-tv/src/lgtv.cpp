493481b79a3b0169396578486a20ebeaefe567bf
Dominik Schlösser <dsc@dosc.net>
2019-12-25 18:56:18 +0100

    Doc fixes

--- lg-tv/src/lgtv.cpp
+++ lg-tv/src/lgtv.cpp
@@ -18,7 +18,8 @@ ustd::Ota ota;
 ustd::TvSerial tv("LG-TV", &Serial1, ustd::TvSerial::TV_SERIAL_TYPE::LG_TV);
 #else // !__ESP32__
 #ifdef USE_SERIAL_DBG
-#error You cannot use define USE_SERIAL_DBG with TvSerial mupplet, since the serial port is needed for the communication with the MP3 hardware!
+#error You cannot use define USE_SERIAL_DBG with TvSerial mupplet, 
+// since the serial port is needed for the communication with the TV hardware!
 #endif // USE_SERIAL_DBG
 ustd::TvSerial tv("TV", (HardwareSerial *)&Serial, ustd::TvSerial::TV_SERIAL_TYPE::LG_TV);
 #endif // !__ESP32__
@@ -28,14 +29,16 @@ void setup() {
     mqtt.begin(&sched);
     ota.begin(&sched);
 
-    /*int tID = */sched.add(appLoop, "main", 1000000); // every 1000000 micro sec = once a second call appLoop
     tv.begin(&sched);
-
     // Use Home Assistant's auto-discovery to register tv in HA as switch.
     tv.registerHomeAssistant("Fernseher", "ESP Serial TV");
+
+    // Currently unused process appLoop:
+    /*int tID = */sched.add(appLoop, "main", 1000000); // every 1000000 micro sec = once a second call appLoop
 }
 
 void appLoop() {
+    // unused. Use this for application logic.
 }
 
 // Never add code to this loop, use appLoop() instead.
