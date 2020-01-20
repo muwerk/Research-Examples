0761158047d2d16a9d5d3211f55c95a46d6d76ee
Dominik Schlösser <dsc@dosc.net>
2020-01-20 18:24:20 +0100

    Initial, not tested

---
+++ butterlamp/README.md
@@ -0,0 +1,15 @@
+## A butterlamp simulator
+
+Simulates two independent butterlamps with realistic random-walk flicker-effects
+using Adafruit Neopixel arrays.
+
+The butterlamps that start burning in the evening and slowly exhaust around midnight.
+
+T.B.D.
+
+## Documentation
+
+* [muwerk scheduler and messaging](https://github.com/muwerk/muwerk)
+* [munet networking and mqtt](https://github.com/muwerk/munet)
+* [documentation of hardware mupplets and supported MQTT messages](https://github.com/muwerk/mupplets)
+* [Home Assistant's auto-discovery for MQTT](https://www.home-assistant.io/docs/mqtt/discovery/)
---
+++ butterlamp/src/butterlamp.cpp
@@ -0,0 +1,53 @@
+#define USE_SERIAL_DBG 1
+
+#include "platform.h"
+#include "scheduler.h"
+
+#include "net.h"
+#include "mqtt.h"
+#include "ota.h"
+#include "../../../munet/web.h"
+
+#include "mup_util.h"
+#include "ldr.h"
+#include "neocandle.h"
+
+void appLoop();
+
+ustd::Scheduler sched(10,16,32);
+ustd::Net net(LED_BUILTIN);
+ustd::Mqtt mqtt;
+ustd::Ota ota;
+ustd::Web web;
+
+ustd::Ldr ldr("ldr", A0);
+ustd::NeoCandle candles("candles", NEOCANDLE_PIN, NEOCANDLE_NUMPIXELS,
+                        NEOCANDLEX_OPTIONS);
+
+void setup() {
+#ifdef USE_SERIAL_DBG
+    Serial.begin(115200);
+    Serial.println("Startup");
+#endif  // USE_SERIAL_DBG
+    net.begin(&sched);
+    mqtt.begin(&sched);
+    ota.begin(&sched);
+    web.begin(&sched);
+
+    int tID = sched.add(appLoop, "main", 1000000); // every 1000000 micro sec = once a second call appLoop
+
+    ldr.begin(&sched);
+    candles.begin(&sched);
+
+    // Use Home Assistant's auto-discovery to register the butterlamp
+    ldr.registerHomeAssistant("Butterlampen-Helligkeitssensor", "Butterlampe");
+    candles.registerHomeAssistant("Butterlampe Eins", "Butterlampe");
+}
+
+void appLoop() {
+}
+
+// Never add code to this loop, use appLoop() instead.
+void loop() {
+    sched.loop();
+}
