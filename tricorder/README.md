cbc8fe7253a02fd57dd8e16ee427fcc55df200da
Dominik Schlösser <dsc@dosc.net>
2020-03-20 16:38:39 +0100

    IR Temperature example

---
+++ tricorder/README.md
@@ -0,0 +1,9 @@
+## Temperature sensor GY-906
+
+t.b.d.
+
+## Documentation
+
+* [muwerk scheduler and messaging](https://github.com/muwerk/muwerk)
+* [munet networking and mqtt](https://github.com/muwerk/munet)
+* [documentation of hardware mupplets and supported MQTT messages](https://github.com/muwerk/mupplets)
---
+++ tricorder/src/tricorder.cpp
@@ -0,0 +1,40 @@
+#define USE_SERIAL_DBG 1
+
+#include "platform.h"
+#include "scheduler.h"
+#include "net.h"
+#include "mqtt.h"
+#include "ota.h"
+
+#include "gy906_temp.h"
+
+void appLoop();
+
+ustd::Scheduler sched(10,16,32);
+ustd::Net net(LED_BUILTIN);
+ustd::Mqtt mqtt;
+ustd::Ota ota;
+
+ustd::Gy906 gy("GY906");
+
+void setup() {
+#ifdef USE_SERIAL_DBG
+    Serial.begin(115200);
+    Serial.println("Startup");
+#endif  // USE_SERIAL_DBG
+    net.begin(&sched);
+    mqtt.begin(&sched);
+    ota.begin(&sched);
+    /* int tID = */ sched.add(appLoop, "main", 1000000);
+    gy.begin(&sched);
+
+    // gy.registerHomeAssistant("Tricorder");
+}
+
+void appLoop() {
+}
+
+// Never add code to this loop, use appLoop() instead.
+void loop() {
+    sched.loop();
+}
