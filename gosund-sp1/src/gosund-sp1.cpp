a8b5bef1e2a244b032efe0a733d5f95425b8cc86
Dominik Schloesser <dsc@dosc.net>
2020-09-27 18:13:49 +0200

    Cleanup

--- gosund-sp1/src/gosund-sp1.cpp
+++ gosund-sp1/src/gosund-sp1.cpp
@@ -12,23 +12,23 @@
 
 void appLoop();
 
-ustd::Scheduler sched(10,16,32);
-ustd::Net net(13); // use red led for network connection phase
+ustd::Scheduler sched(10, 16, 32);
+ustd::Net net(13);  // use red led for network connection phase
 ustd::Mqtt mqtt;
 ustd::Ota ota;
 
-ustd::Led led1("Led1",1,false); // blue
-ustd::Led led2("Led2",13,false); // red
-ustd::Switch toggleswitch("mySwitch",3, ustd::Switch::Mode::Flipflop, false);
-ustd::DigitalOut relay("Relay",14,true);
-ustd::PowerBl0937 power("Power",4,5,12,0,1);
+ustd::Led led1("Led1", 1, false);   // blue
+ustd::Led led2("Led2", 13, false);  // red
+ustd::Switch toggleswitch("mySwitch", 3, ustd::Switch::Mode::Flipflop, false);
+ustd::DigitalOut relay("Relay", 14, true);
+ustd::PowerBl0937 power("Power", 4, 5, 12, 0, 1);
 
 void switch_messages(String topic, String msg, String originator) {
     if (topic == "mySwitch/switch/state") {
-        if (msg=="on") {
+        if (msg == "on") {
             led1.set(true);
             relay.set(true);
-        } else if (msg=="off") {
+        } else if (msg == "off") {
             led1.set(false);
             relay.set(false);
         }
@@ -39,18 +39,22 @@ void setup() {
     net.begin(&sched, false);  // false: Never reboot on net failure
     mqtt.begin(&sched);
     ota.begin(&sched);
-    int tID = sched.add(appLoop, "main", 1000000);
+    /*int tID = */ sched.add(appLoop, "main", 1000000);
     led1.begin(&sched);
     led2.begin(&sched);
     toggleswitch.begin(&sched);
     relay.begin(&sched);
     power.begin(&sched);
-    power.setUserCalibrationFactors(1.0,1.0,1.0); // calib. factors for power (W), voltage (V) and current (A)
+    power.setUserCalibrationFactors(
+        1.0, 1.0,
+        1.0);  // calib. factors for power (W), voltage (V) and current (A)
 
     String friendlyName;
-    if (!ustd::readFriendlyName(friendlyName)) friendlyName="Gosund SP1";
-    toggleswitch.registerHomeAssistant(friendlyName, friendlyName, "mdi:power-socket-de");
-    power.registerHomeAssistant(friendlyName+" power", friendlyName);
+    if (!ustd::readFriendlyName(friendlyName))
+        friendlyName = "Gosund SP1";
+    toggleswitch.registerHomeAssistant(friendlyName, friendlyName,
+                                       "mdi:power-socket-de");
+    power.registerHomeAssistant(friendlyName + " power", friendlyName);
 
     sched.subscribe(tID, "mySwitch/switch/state", switch_messages);
 }
