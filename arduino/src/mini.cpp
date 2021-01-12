d35b5ad0304f4259f9997751fc7cbf39c966085a
Dominik Schlösser <dsc@dosc.net>
2021-01-12 12:05:16 +0100

    Test code for new serial console

--- arduino/src/mini.cpp
+++ arduino/src/mini.cpp
@@ -3,17 +3,27 @@
 #include "platform.h"
 #include "scheduler.h"
 
-//#include "net.h"
-//#include "ota.h"
-//#include "mqtt.h"
+#ifdef __ESP__
+#include "net.h"
+#include "ota.h"
+#include "mqtt.h"
+#endif
+
 #include "console.h"
 
 // entities for core functionality
+#if defined(__ESP__) || defined(__ATMEGA__)
+ustd::Scheduler sched(10, 16, 32);
+#else
 ustd::Scheduler sched(2, 2, 2);
+#endif
 ustd::Console con;
-// ustd::Net       net( LED_BUILTIN );
-// ustd::Ota       ota;
-// ustd::Mqtt      mqtt;
+
+#ifdef __ESP__
+ustd::Net net(LED_BUILTIN);
+ustd::Ota ota;
+ustd::Mqtt mqtt;
+#endif
 
 // main application command handler
 void app_command_handler(String topic, String msg, String originator) {
@@ -35,7 +45,7 @@ void app() {
 // application setup
 void setup() {
     DBG_INIT(115200);
-    // DBG( "\nStartup: " + WiFi.macAddress() );
+    DBG("\nStartup: " + WiFi.macAddress());
 
     // extend console
     /*
@@ -48,14 +58,16 @@ void setup() {
     */
     // initialize core components
     con.begin(&sched, "", 115200);
-    // net.begin( &sched );
-    // ota.begin( &sched );
-    // mqtt.begin( &sched, "", "", "" );
+#ifdef __ESP__
+    net.begin(&sched);
+    ota.begin(&sched);
+    mqtt.begin(&sched);
+#endif
 
     // initialize application
 
     int tID = sched.add(app, "main", 50000);
-    // sched.subscribe(tID, "cmnd/#", app_command_handler);
+    sched.subscribe(tID, "cmnd/#", app_command_handler);
 }
 
 // application runtime
