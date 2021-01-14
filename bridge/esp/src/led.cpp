a809153e490417042305fd8dd7a36a9fd6d267b1
Dominik Schlösser <dsc@dosc.net>
2021-01-14 17:00:56 +0100

    test samples

--- bridge/esp/src/led.cpp
+++ bridge/esp/src/led.cpp
@@ -1,3 +1,5 @@
+#define USB_SERIAL_DBG 1
+
 #include "platform.h"
 #include "scheduler.h"
 
@@ -11,26 +13,35 @@
 void appLoop();
 
 ustd::Scheduler sched(10, 16, 32);
-ustd::MuSerial muser("node1", &Serial);
+ustd::MuSerial muser("arduino", &Serial2);
 
 #ifdef __ESP__
 ustd::Net net(LED_BUILTIN);
 ustd::Mqtt mqtt;
-// ustd::Ota ota;
+ustd::Ota ota;
 #endif
 
 void setup() {
+    pinMode(LED_BUILTIN, OUTPUT);
     muser.begin(&sched);
 #ifdef __ESP__
     net.begin(&sched);
     mqtt.begin(&sched);
-//    ota.begin(&sched);
+    ota.begin(&sched);
 #endif
     sched.add(appLoop, "main", 1000000);
     // led.setMode(led.Mode::Wave,1000);
 }
 
+bool bl = false;
 void appLoop() {
+    if (bl) {
+        bl = false;
+        digitalWrite(LED_BUILTIN, HIGH);
+    } else {
+        bl = true;
+        digitalWrite(LED_BUILTIN, LOW);
+    }
 }
 
 // Never add code to this loop, use appLoop() instead.
