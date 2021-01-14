e5986501578d37b5d946eccf4d95c3359dd0459b
Dominik Schlösser <dsc@dosc.net>
2021-01-14 17:05:07 +0100

    counterpart

---
+++ bridge/ardu/README.md
@@ -0,0 +1,1 @@
+## Serial MQTT bridge between Arduino and Esp8266 - ESP part
---
+++ bridge/ardu/src/led.cpp
@@ -0,0 +1,39 @@
+#include "platform.h"
+#include "scheduler.h"
+
+#include "muserial.h"
+#ifdef __ESP__
+#include "net.h"
+#include "mqtt.h"
+#include "ota.h"
+#endif
+
+void appLoop();
+
+ustd::Scheduler sched(10, 16, 32);
+ustd::MuSerial muser("esp12e", &Serial);
+
+#ifdef __ESP__
+ustd::Net net(LED_BUILTIN);
+ustd::Mqtt mqtt;
+// ustd::Ota ota;
+#endif
+
+void setup() {
+    muser.begin(&sched);
+#ifdef __ESP__
+    net.begin(&sched);
+    mqtt.begin(&sched);
+//    ota.begin(&sched);
+#endif
+    sched.add(appLoop, "main", 1000000);
+    // led.setMode(led.Mode::Wave,1000);
+}
+
+void appLoop() {
+}
+
+// Never add code to this loop, use appLoop() instead.
+void loop() {
+    sched.loop();
+}
